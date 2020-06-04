56090e04f94f009986ac12f63f66b279048f30da
islemkehoul <islemkehoul98@gmail.com>
2020-06-05 00:53:15 +0400

    stack

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,52 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      backgroundColor: Colors.grey[200],
+      body : Container(
+        padding: EdgeInsets.fromLTRB(10, 35, 10, 10),
+        child : Column(
+          
+          children: <Widget>[
+            
+            Container(
+              height: 300,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(40),
+                image : DecorationImage(
+                  image:AssetImage("assets/purple.jpg"), 
+                  
+                ),
+                boxShadow: [
+      BoxShadow(
+        color: Colors.grey.withOpacity(0.25),
+        spreadRadius: 2,
+        blurRadius: 8,
+        offset: Offset(-2, -2), // changes position of shadow
+      ),
+    ],
+              ),
+              child: Stack(
+                children: <Widget>[
+                Positioned(
+                  width : 80,
+                  height : 200,
+                  child : Container(
+                    decoration: BoxDecoration(
+                      image :DecorationImage(image: AssetImage("assets/download.png"),
+                      
+                    )
+                       
+                    ),
+                     )
+                )
+               
+                ],
+              ),
+            ),
+    
+          ],
+        )
+      ),
     );
   }
 }
