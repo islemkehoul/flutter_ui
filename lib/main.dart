44ca6aca982abb4f4c209cbd73c6824358c63d50
islemkehoul <islemkehoul98@gmail.com>
2020-06-05 17:11:52 +0400

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -11,52 +12,33 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[200],
+        backgroundColor: Colors.transparent,
+       
+      
       body : Container(
-        padding: EdgeInsets.fromLTRB(10, 35, 10, 10),
-        child : Column(
-          
-          children: <Widget>[
-            
-            Container(
-              height: 300,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(40),
-                image : DecorationImage(
-                  image:AssetImage("assets/purple.jpg"), 
-                  
-                ),
-                boxShadow: [
-      BoxShadow(
-        color: Colors.grey.withOpacity(0.25),
-        spreadRadius: 2,
-        blurRadius: 8,
-        offset: Offset(-2, -2), // changes position of shadow
-      ),
-    ],
-              ),
-              child: Stack(
-                children: <Widget>[
-                Positioned(
-                  width : 80,
-                  height : 200,
-                  child : Container(
-                    decoration: BoxDecoration(
-                      image :DecorationImage(image: AssetImage("assets/download.png"),
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/universe.jpg"), fit: BoxFit.cover)),
+                child: Stack(
+                  children: <Widget>[
+
+                  Container(
+                    
+                    child: Text(
+                      'modish login form',
                       
-                    )
-                       
-                    ),
-                     )
-                )
-               
-                ],
-              ),
-            ),
-    
-          ],
-        )
-      ),
+                      style:
+                        GoogleFonts.aladin(
+                          fontSize: 30,
+                          color: Colors.red
+                      ),
+                  ),
+                  ),
+                  
+                  ],
+                ),
+                ),
+                
     );
   }
 }
